740455780c489daaa3a9a6251b7bccc24fd8fe38
User Name <Email@example.com>
2021-10-29 23:58:20 +0300

    Create log_in.Designer.cs

---
+++ Dental clinic management/Ui/log_in.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Dental_clinic_management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.UserName = new System.Windows.Forms.TextBox();
+            this.PassWord = new System.Windows.Forms.TextBox();
+            this.login = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Name = "textBox1";
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+            this.tableLayoutPanel1.BackgroundImage = global::Dental_clinic_management.Properties.Resources.Untitled_12;
+            this.tableLayoutPanel1.Controls.Add(this.UserName, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.PassWord, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.login, 1, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
+            // 
+            // UserName
+            // 
+            resources.ApplyResources(this.UserName, "UserName");
+            this.UserName.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.UserName.Name = "UserName";
+            this.UserName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.UserName_KeyDown_1);
+            // 
+            // PassWord
+            // 
+            resources.ApplyResources(this.PassWord, "PassWord");
+            this.PassWord.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.PassWord.Name = "PassWord";
+            this.PassWord.UseSystemPasswordChar = true;
+            this.PassWord.KeyDown += new System.Windows.Forms.KeyEventHandler(this.PassWord_KeyDown);
+            // 
+            // login
+            // 
+            resources.ApplyResources(this.login, "login");
+            this.login.BackgroundImage = global::Dental_clinic_management.Properties.Resources.botton;
+            this.login.FlatAppearance.BorderSize = 0;
+            this.login.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.login.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.login.ForeColor = System.Drawing.SystemColors.Control;
+            this.login.Name = "login";
+            this.login.UseVisualStyleBackColor = true;
+            this.login.Click += new System.EventHandler(this.login_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox UserName;
+        private System.Windows.Forms.TextBox PassWord;
+        private System.Windows.Forms.Button login;
+    }
+}
+
