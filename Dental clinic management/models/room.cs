dfe5f1a3c33184fc1b56c382b642d009f0636db8
User Name <Email@example.com>
2021-10-29 23:51:31 +0300

    Create room.cs

---
+++ Dental clinic management/models/room.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Dental_clinic_management
+{
+    class room
+    {
+        public int room_id { get; private set; }
+        public String room_name { get; private set; }
+        public String roo_status { get; private set; }
+
+
+        public room(int room_id, String room_name, String roo_status)
+        {
+            this.room_id = room_id;
+            this.room_name = room_name;
+            this.roo_status = roo_status;
+        }
+    }
+}
