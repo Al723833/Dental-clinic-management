0bad82277a03e42c0bf7aaa7a3187e010eefe025
User Name <Email@example.com>
2021-10-29 23:51:28 +0300

    Create Period.cs

---
+++ Dental clinic management/models/Period.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dental_clinic_management
+{
+    class Period
+    {
+        public int Period_id { get; set; }
+        public String Period_name { get; private set; }
+        public String start_time { get; private set; }
+        public String end_time { get; private set; }
+
+
+        public Period(int Period_id, String Period_name, String start_time, String end_time)
+        {
+            this.Period_id = Period_id;
+            this.Period_name = Period_name;
+            this.start_time = start_time;
+            this.end_time = end_time;
+
+        }
+    }
+}
