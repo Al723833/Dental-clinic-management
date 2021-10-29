0372a3178d6d8edd00a4c2953feae59b6308877f
User Name <Email@example.com>
2021-10-29 23:51:16 +0300

    Create Operations.cs

---
+++ Dental clinic management/models/Operations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dental_clinic_management
+{
+    class Operations
+    {
+        public int Operations_id { get; set; }
+        public String Operations_name { get; set; }
+
+        public Operations(int Operations_id, String Operations_name)
+        {
+            this.Operations_id = Operations_id;
+            this.Operations_name = Operations_name;
+        }
+    }
+}
