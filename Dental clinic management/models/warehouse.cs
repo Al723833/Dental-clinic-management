bb88accb8a5c524ae4bb598b22024a5a6b2302a9
User Name <Email@example.com>
2021-10-29 23:51:46 +0300

    Create warehouse.cs

---
+++ Dental clinic management/models/warehouse.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dental_clinic_management
+{
+    class warehouse
+    {
+        public int warehouse_id { get; set; }
+        public String warehouse_name { get; set; }
+        public String mobile_number { get; set; }
+        public String warehouse_address { get; set; }
+
+        public warehouse(int warehouse_id, String warehouse_name, String mobile_number, String warehouse_address)
+        {
+            this.warehouse_id = warehouse_id;
+            this.warehouse_name = warehouse_name;
+            this.mobile_number = mobile_number;
+            this.warehouse_address = warehouse_address;
+
+        }
+    }
+}
