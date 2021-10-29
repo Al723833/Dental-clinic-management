a0c22e9faa7415d4522ecf635f0b51350ee8e630
User Name <Email@example.com>
2021-10-29 23:50:48 +0300

    Create Doctor.cs

---
+++ Dental clinic management/models/Doctor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Dental_clinic_management
+{
+    class Doctor
+    {
+        public int Doctor_id { get; set; }
+        public String Doctor_name { get; set; }
+
+        public Doctor(int Doctor_id, String Doctor_name)
+        {
+            this.Doctor_id = Doctor_id;
+            this.Doctor_name = Doctor_name;
+        }
+    }
+}
