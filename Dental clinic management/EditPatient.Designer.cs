2535c95d198f2fde5c105f054e97705c4a719786
User Name <Email@example.com>
2021-10-29 23:49:42 +0300

    Create EditPatient.Designer.cs

---
+++ Dental clinic management/EditPatient.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Dental_clinic_management
+{
+    partial class EditPatient
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // editpationt1
+            // 
+            this.editpationt1.BackColor = System.Drawing.Color.Transparent;
+            this.editpationt1.Location = new System.Drawing.Point(0, -1);
+            this.editpationt1.Name = "editpationt1";
+            this.editpationt1.Size = new System.Drawing.Size(778, 783);
+            this.editpationt1.TabIndex = 0;
+            // 
+            // EditPatient
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(777, 771);
+            this.Controls.Add(this.editpationt1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "EditPatient";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Editpationt editpationt1;
+    }
+}
