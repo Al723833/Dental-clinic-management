579f036a4b5c24827c6c77f1feeb26647ca2100c
User Name <Email@example.com>
2021-10-29 23:49:38 +0300

    Create EditPatient.cs

---
+++ Dental clinic management/EditPatient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dental_clinic_management
+{
+    public partial class EditPatient : Form
+    {
+        public EditPatient(Patient_view frm, int indexss)
+        {
+            this.editpationt1 = new Dental_clinic_management.Editpationt(frm, indexss);
+            InitializeComponent();
+            this.TransparencyKey = Color.Turquoise;
+            this.BackColor = Color.Turquoise;
+        }
+    }
+}
