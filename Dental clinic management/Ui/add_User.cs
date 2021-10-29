fa87e7269237aac3fc5657bd428d8ef545b68a6e
User Name <Email@example.com>
2021-10-29 23:57:37 +0300

    Create add_User.cs

---
+++ Dental clinic management/Ui/add_User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dental_clinic_management
+{
+    public partial class add_User : Form
+    {
+        public add_User()
+        {
+            InitializeComponent();
+
+            this.BackColor = Color.LimeGreen;
+            this.TransparencyKey = Color.LimeGreen;
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+    }
+}
