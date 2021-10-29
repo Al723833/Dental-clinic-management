56113f2fec49297145e5d4434551fe2c8f9bf9c0
User Name <Email@example.com>
2021-10-29 23:51:41 +0300

    Create Teeth.cs

---
+++ Dental clinic management/models/Teeth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dental_clinic_management
+{
+    class Teeth
+    {
+        public int Teeth_id { get; set; }
+        public String Teeth_name { get; set; }
+
+        public Teeth(int Teeth_id, String Teeth_name)
+        {
+            this.Teeth_id = Teeth_id;
+            this.Teeth_name = Teeth_name;
+        }
+    }
+}
