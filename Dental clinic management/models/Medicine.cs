188c73c79af358aab8d987a5ba45416d21222418
User Name <Email@example.com>
2021-10-29 23:51:08 +0300

    Create Medicine.cs

---
+++ Dental clinic management/models/Medicine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dental_clinic_management
+{
+    class Medicine
+    {
+        public int Medicine_id { get; set; }
+        public String Medicine_name { get; set; }
+
+        public Medicine(int Medicine_id, String Medicine_name)
+        {
+            this.Medicine_id = Medicine_id;
+            this.Medicine_name = Medicine_name;
+
+        }
+    }
+}
