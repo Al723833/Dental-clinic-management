ae7de8315c87ea46e88e374d981c44bce892f536
User Name <Email@example.com>
2021-10-29 23:57:55 +0300

    Create mostod3at_element.designer.cs

---
+++ Dental clinic management/Ui/designElements/mostod3at_element.designer.cs
@@ -0,0 +1,216 @@
+﻿
+namespace Dental_clinic_management
+{
+    partial class mostod3at_element
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mostod3at_element));
+            this.name = new System.Windows.Forms.Label();
+            this.mobileNember = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.تعديلToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.حذفToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.guna2CircleButton2 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.guna2CirclePictureBox8 = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.guna2CircleButton1 = new Guna.UI2.WinForms.Guna2CircleButton();
+            this.address = new System.Windows.Forms.Label();
+            this.ID_PATIENT = new System.Windows.Forms.Label();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox8)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.BackColor = System.Drawing.Color.Transparent;
+            this.name.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F);
+            this.name.Location = new System.Drawing.Point(730, 20);
+            this.name.Name = "name";
+            this.name.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.name.Size = new System.Drawing.Size(93, 25);
+            this.name.TabIndex = 9;
+            this.name.Text = "الإسم رباعي";
+            this.name.MouseLeave += new System.EventHandler(this.name_MouseLeave);
+            this.name.MouseHover += new System.EventHandler(this.name_MouseHover);
+            // 
+            // mobileNember
+            // 
+            this.mobileNember.AutoSize = true;
+            this.mobileNember.BackColor = System.Drawing.Color.Transparent;
+            this.mobileNember.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F);
+            this.mobileNember.Location = new System.Drawing.Point(512, 20);
+            this.mobileNember.Name = "mobileNember";
+            this.mobileNember.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.mobileNember.Size = new System.Drawing.Size(80, 25);
+            this.mobileNember.TabIndex = 42;
+            this.mobileNember.Text = "رقم الجوال";
+            this.mobileNember.MouseLeave += new System.EventHandler(this.label6_MouseLeave);
+            this.mobileNember.MouseHover += new System.EventHandler(this.label6_MouseHover);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.تعديلToolStripMenuItem,
+            this.حذفToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(116, 52);
+            // 
+            // تعديلToolStripMenuItem
+            // 
+            this.تعديلToolStripMenuItem.Name = "تعديلToolStripMenuItem";
+            this.تعديلToolStripMenuItem.Size = new System.Drawing.Size(115, 24);
+            this.تعديلToolStripMenuItem.Text = "تعديل";
+            // 
+            // حذفToolStripMenuItem
+            // 
+            this.حذفToolStripMenuItem.Name = "حذفToolStripMenuItem";
+            this.حذفToolStripMenuItem.Size = new System.Drawing.Size(115, 24);
+            this.حذفToolStripMenuItem.Text = "حذف";
+            // 
+            // guna2CircleButton2
+            // 
+            this.guna2CircleButton2.CheckedState.Parent = this.guna2CircleButton2;
+            this.guna2CircleButton2.ContextMenuStrip = this.contextMenuStrip1;
+            this.guna2CircleButton2.CustomImages.Parent = this.guna2CircleButton2;
+            this.guna2CircleButton2.DisabledState.Parent = this.guna2CircleButton2;
+            this.guna2CircleButton2.FillColor = System.Drawing.Color.White;
+            this.guna2CircleButton2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton2.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton2.HoverState.Parent = this.guna2CircleButton2;
+            this.guna2CircleButton2.Image = global::Dental_clinic_management.Properties.Resources.delete__1_;
+            this.guna2CircleButton2.Location = new System.Drawing.Point(7, 6);
+            this.guna2CircleButton2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.guna2CircleButton2.Name = "guna2CircleButton2";
+            this.guna2CircleButton2.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton2.ShadowDecoration.Parent = this.guna2CircleButton2;
+            this.guna2CircleButton2.Size = new System.Drawing.Size(50, 50);
+            this.guna2CircleButton2.TabIndex = 47;
+            this.guna2CircleButton2.Click += new System.EventHandler(this.guna2CircleButton2_Click);
+            // 
+            // guna2CirclePictureBox8
+            // 
+            this.guna2CirclePictureBox8.BackColor = System.Drawing.Color.Transparent;
+            this.guna2CirclePictureBox8.Image = ((System.Drawing.Image)(resources.GetObject("guna2CirclePictureBox8.Image")));
+            this.guna2CirclePictureBox8.ImageRotate = 0F;
+            this.guna2CirclePictureBox8.Location = new System.Drawing.Point(935, 12);
+            this.guna2CirclePictureBox8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.guna2CirclePictureBox8.Name = "guna2CirclePictureBox8";
+            this.guna2CirclePictureBox8.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CirclePictureBox8.ShadowDecoration.Parent = this.guna2CirclePictureBox8;
+            this.guna2CirclePictureBox8.Size = new System.Drawing.Size(42, 44);
+            this.guna2CirclePictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2CirclePictureBox8.TabIndex = 39;
+            this.guna2CirclePictureBox8.TabStop = false;
+            this.guna2CirclePictureBox8.MouseLeave += new System.EventHandler(this.guna2CirclePictureBox8_MouseLeave);
+            this.guna2CirclePictureBox8.MouseHover += new System.EventHandler(this.guna2CirclePictureBox8_MouseHover);
+            // 
+            // guna2CircleButton1
+            // 
+            this.guna2CircleButton1.CheckedState.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.ContextMenuStrip = this.contextMenuStrip1;
+            this.guna2CircleButton1.CustomImages.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.DisabledState.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.FillColor = System.Drawing.Color.White;
+            this.guna2CircleButton1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2CircleButton1.ForeColor = System.Drawing.Color.White;
+            this.guna2CircleButton1.HoverState.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.Image = global::Dental_clinic_management.Properties.Resources.edit;
+            this.guna2CircleButton1.Location = new System.Drawing.Point(63, 6);
+            this.guna2CircleButton1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.guna2CircleButton1.Name = "guna2CircleButton1";
+            this.guna2CircleButton1.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.guna2CircleButton1.ShadowDecoration.Parent = this.guna2CircleButton1;
+            this.guna2CircleButton1.Size = new System.Drawing.Size(50, 50);
+            this.guna2CircleButton1.TabIndex = 8;
+            this.guna2CircleButton1.Click += new System.EventHandler(this.guna2CircleButton1_Click);
+            // 
+            // address
+            // 
+            this.address.AutoSize = true;
+            this.address.BackColor = System.Drawing.Color.Transparent;
+            this.address.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F);
+            this.address.Location = new System.Drawing.Point(276, 22);
+            this.address.Name = "address";
+            this.address.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.address.Size = new System.Drawing.Size(94, 25);
+            this.address.TabIndex = 49;
+            this.address.Text = "عنوان السكن";
+            // 
+            // ID_PATIENT
+            // 
+            this.ID_PATIENT.AutoSize = true;
+            this.ID_PATIENT.BackColor = System.Drawing.Color.Transparent;
+            this.ID_PATIENT.Font = new System.Drawing.Font("Franklin Gothic Medium", 12F);
+            this.ID_PATIENT.Location = new System.Drawing.Point(1274, 20);
+            this.ID_PATIENT.Name = "ID_PATIENT";
+            this.ID_PATIENT.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ID_PATIENT.Size = new System.Drawing.Size(0, 25);
+            this.ID_PATIENT.TabIndex = 51;
+            this.ID_PATIENT.Visible = false;
+            // 
+            // mostod3at_element
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Controls.Add(this.ID_PATIENT);
+            this.Controls.Add(this.address);
+            this.Controls.Add(this.guna2CircleButton2);
+            this.Controls.Add(this.mobileNember);
+            this.Controls.Add(this.guna2CirclePictureBox8);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.guna2CircleButton1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "mostod3at_element";
+            this.Size = new System.Drawing.Size(996, 68);
+            this.Load += new System.EventHandler(this.rowpationt_Load);
+            this.MouseLeave += new System.EventHandler(this.rowpationt_MouseLeave);
+            this.MouseHover += new System.EventHandler(this.rowpationt_MouseHover);
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.guna2CirclePictureBox8)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton1;
+        private System.Windows.Forms.Label name;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox guna2CirclePictureBox8;
+        private System.Windows.Forms.Label mobileNember;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem تعديلToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem حذفToolStripMenuItem;
+        private Guna.UI2.WinForms.Guna2CircleButton guna2CircleButton2;
+        private System.Windows.Forms.Label address;
+        private System.Windows.Forms.Label ID_PATIENT;
+    }
+}
