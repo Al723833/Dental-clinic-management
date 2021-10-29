337380bf3d596bac63cbe150e2396abcfdb822d8
User Name <Email@example.com>
2021-10-29 23:50:44 +0300

    Create Admin.cs

---
+++ Dental clinic management/models/Admin.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dental_clinic_management
+{
+    class Admin
+    {
+        public String admin_name { get; private set; }
+        public String admin_pass { get; private set; }
+        public String permission { get; private set; }
+
+
+        public Admin(String admin_name, String admin_pass, String permission)
+        {
+            this.admin_name = admin_name;
+            this.admin_pass = admin_pass;
+            this.permission = permission;
+        }
+
+    }
+}
