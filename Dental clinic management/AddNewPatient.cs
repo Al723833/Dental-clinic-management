b55b5113bf361d6d114b8539869984c34769e0f2
User Name <Email@example.com>
2021-10-29 23:46:21 +0300

    Create AddNewPatient.cs

---
+++ Dental clinic management/AddNewPatient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dental_clinic_management
+{
+    public partial class AddNewPatient : Form
+    {
+
+        public AddNewPatient(Patient_view frm)
+        {
+            this.addpationt1 = new Dental_clinic_management.addpationt(frm);
+            InitializeComponent();
+            this.TransparencyKey = Color.Turquoise;
+            this.BackColor = Color.Turquoise;
+        }
+    }
+}
